adbaf19f3f25354b97e914540f5588ddce484d11
Sanila <sanilas@smartrabbitz.com>
2021-11-06 15:50:35 +0530

    Added Assignment-10

---
+++ Assignment_10/popup.js
@@ -0,0 +1,28 @@
+
+function openmodal()
+{
+    var modal = document.getElementById("myModal");
+    var btn = document.getElementById("myBtn");
+    var span = document.getElementsByClassName("close")[0];
+
+    modal.style.display = "block";
+
+    span.onclick = function()
+    {
+        modal.style.display = "none";
+    }
+
+    document.getElementById("closeBtn").onclick = function()
+    {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) 
+    {
+        if (event.target == modal) 
+        {
+            modal.style.display = "none";
+        }
+    }
+}
+
